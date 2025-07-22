59ef395e54496fe799fbf9c13385cb02444cba82
aryan210801 <110852048+aryan210801@users.noreply.github.com>
2025-07-22 20:50:28 +0530

    some improvements used list instead of []

--- src/main/java/com/example/AreaByPinCode/service/PinCodeService.java
+++ src/main/java/com/example/AreaByPinCode/service/PinCodeService.java
@@ -3,6 +3,7 @@ package com.example.AreaByPinCode.service;
 import com.example.AreaByPinCode.model.PinCode;
 import com.example.AreaByPinCode.model.PinCodeDto;
 import com.example.AreaByPinCode.model.PostOfficeResponse;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,17 +18,17 @@ public class PinCodeService {
     public ResponseEntity<List<PostOfficeResponse>> getInfo(String pincode){
         String url = "https://api.postalpincode.in/pincode/" + pincode;
         HttpHeaders headers = new HttpHeaders();
-        headers.set("User-Agent", "Mozilla/5.0"); // ✅ Required for many APIs
+        headers.set("User-Agent", "Mozilla/5.0");
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<PinCode[]> response = restTemplate.exchange(url,
+        ResponseEntity<List<PinCode>> response = restTemplate.exchange(url,
                 HttpMethod.GET,
                 entity,
-                PinCode[].class);
-        PinCode[] allData = response.getBody();
-        if (allData != null && allData.length > 0 && allData[0].getPostOfficeResponse() != null) {
-            List<PostOfficeResponse> postOfficeResponses = allData[0].getPostOfficeResponse();
+                new ParameterizedTypeReference<List<PinCode>>() {});
+        List<PinCode> allData = response.getBody();
+        if (allData != null && !allData.isEmpty() && allData.get(0).getPostOfficeResponse() != null) {
+            List<PostOfficeResponse> postOfficeResponses = allData.get(0).getPostOfficeResponse();
 //            List<PinCodeDto> finalData = postOfficeResponses.stream().map(po -> new PinCodeDto(po.getName(), po.getDistrict(), po.getState()))
 //                    .collect(Collectors.toList());
             return new ResponseEntity<>(postOfficeResponses, HttpStatus.OK);
